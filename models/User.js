d495dd8146e0b38f08391ec8fef864ff7f986d8f
qldirr <sub05073@naver.com>
2024-08-30 16:51:50 +0900

    Feat: 모델 관계 설정

--- models/User.js
+++ models/User.js
@@ -27,10 +27,12 @@ const userModel = (sequelize, DataTypes) => {
       phoneNum: {
         type: DataTypes.STRING(15),
         allowNull: false,
+        unique: true,
       },
       email: {
         type: DataTypes.STRING(30),
         allowNull: false,
+        unique: true,
       },
       profileImg: {
         type: DataTypes.STRING(100),
