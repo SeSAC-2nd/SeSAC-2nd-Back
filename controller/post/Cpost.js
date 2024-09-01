d99845e7c7164fbace3086bf184a8e1e57bb9451
qldirr <sub05073@naver.com>
2024-09-01 17:12:11 +0900

    Feat: 판매글 관련 router,controller 작성 중

--- controller/post/Cpost.js
+++ controller/post/Cpost.js
@@ -1,11 +1,37 @@
-const { Post, ProductImage } = require("../../models/index");
+const { Post, ProductImage, Seller, User } = require("../../models/index");
+
+// 전체 판매글 목록(정렬 포함)
+exports.getPostListPage = async (req, res) => {
+  try {
+    const { page, limit, categoryId } = req.params;
+
+    const pageNumber = page ? parseInt(page, 10) : 1;
+    const pageSize = limit ? parseInt(limit, 10) : 12;
+    const offset = (pageNumber - 1) * pageSize;
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+// 판매글 검색 결과 목록
+exports.getSearchResultsPage = async (req, res) => {
+  try {
+    const { page, limit } = req.params;
+    const { postTitle } = req.query;
+
+    const pageNumber = page ? parseInt(page, 10) : 1;
+    const pageSize = limit ? parseInt(limit, 10) : 12;
+    const offset = (pageNumber - 1) * pageSize;
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 // 판매글 등록
 exports.insertPost = async (req, res) => {
   try {
-    // 판매자 정보 등록 확인
-    // 블랙리스트 여부 확인
-
     // sellerId는 session에서 가져오기
     const {
       sellerId,
@@ -32,6 +58,7 @@ exports.insertPost = async (req, res) => {
     // res.json(newPost);
     if (newPost) {
       const newProductImg = await ProductImage.create({
+        postId: newPost.postId,
         imgName: "image.jpg",
         isThumbnail,
       });
@@ -44,8 +71,36 @@ exports.insertPost = async (req, res) => {
   }
 };
 
-// 판매글 조회
+// 판매글 작성 페이지 이동
+exports.getPostCreatePage = async (req, res) => {
+  // 판매자 정보 등록 확인
+  // 블랙리스트 여부 확인
+};
+
+// 판매글 상세 페이지
+exports.getPostDetailPage = async (req, res) => {
+  try {
+    const { postId } = req.params;
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 // 판매글 수정
+exports.updatePost = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 // 판매글 삭제
+exports.deletePost = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
