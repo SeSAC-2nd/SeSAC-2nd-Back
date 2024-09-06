2953ae581d33e1713e79be240d731f4aab2892c9
qldirr <sub05073@naver.com>
2024-09-06 12:51:36 +0900

    Fix: 로그인 post 방식으로 변경

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,32 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../../controller/user/Cuser');
+const controller = require("../../controller/user/Cuser");
 
 // 로그인
-router.get('/login', controller.userLogin);
+router.post("/login", controller.userLogin);
 
 // 회원가입
-router.post('/register', controller.userRegister);
+router.post("/register", controller.userRegister);
 
 // 로그인 아이디 중복 체크
-router.post('/checkLoginid', controller.checkDuplicatedLoginid);
+router.post("/checkLoginid", controller.checkDuplicatedLoginid);
 
 // 로그인 닉네임 중복 체크
-router.post('/checkNickname', controller.checkDuplicatedNickname);
+router.post("/checkNickname", controller.checkDuplicatedNickname);
 
 // 비밀번호 일치 확인
-router.post('/checkPassword', controller.checkPassword);
+router.post("/checkPassword", controller.checkPassword);
 
 // 로그아웃
-router.get('/logout', controller.userLogout);
+router.get("/logout", controller.userLogout);
 
 // 회원 정보 수정
-router.patch('/:userId', controller.updateUser);
+router.patch("/:userId", controller.updateUser);
 
 // 회원 조회
-router.get('/:userId', controller.getUser);
+router.get("/:userId", controller.getUser);
 
 // 회원 탈퇴
-router.patch('/delete/:userId', controller.deleteUser);
+router.patch("/delete/:userId", controller.deleteUser);
 
 module.exports = router;
