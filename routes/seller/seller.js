93d115565acc13514a665174ee4032f55c790733
Yang tae wan <137491948+behindy3359@users.noreply.github.com>
2024-09-06 17:57:26 +0900

    Fix : now we can file upload

--- routes/seller/seller.js
+++ routes/seller/seller.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const controller = require('../../controller/seller/Cseller');
 
 // 판매자 등록
-router.post('/', controller.insertSeller);
+router.post('/', upload.single('seller-img'), controller.insertSeller);
 
 // 판매자 조회
 router.get('/:sellerId', controller.getSeller);
 
 // 판매자 수정
-router.patch('/:sellerId', controller.updateSeller);
+router.patch('/:sellerId', upload.single('seller-img'), controller.updateSeller);
 
 // 판매자 삭제
 router.patch('/delete/:sellerId', controller.deleteSeller);
